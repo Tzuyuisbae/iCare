0069bdf54498c41b49319460556e2babe11c39b2
Jia Xun Liu <liuji112@IITS-B473-39.utsc-labs.utoronto.ca>
2018-11-20 13:34:14 -0500

    added confirm button and input bars for the passwords

--- D4/iCare/src/pages/ChangePasswordPage.js
+++ D4/iCare/src/pages/ChangePasswordPage.js
@@ -7,19 +7,86 @@ import {Link} from "react-router-dom";
 
 export default class ChangePasswordPage extends React.Component {
 
+    state = 
+    {
+        oldPassword: '',
+        newPassword: '',
+        retypeNewPassword: '',
+        canchange: false,
+        data : {},
+    }
+
+    updateChangePasswordPage = e => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.setState({
+            oldPassword: '',
+            NewPassword: '',
+            retypeNewpassword: ''
+        });
+
+        axios.post('http://localhost:8000/authenticate', 
+        {'email':this.props.username, 'password':this.state.oldPassword},
+        {headers :{'Content-Type': 'application/json'}})
+        .then(res => {
+            this.setState({ data: res.data });
+            console.log(this.state.data);
+            if(this.state.data.authenticated){
+                this.props.history.push("/ChangePasswordForm")
+                
+                // change the password in the database
+
+
+            }
+            else{
+                console.log('failed');
+            }
+        })
+    };
+
     render() {
+        const place = !this.state.canchange ? '/form' : '/ChangePasswordForm';
         return (
-            <div className="ChangePassword">
-                <h1>upload page is here haha yeet</h1>
-                <Link to={'/'}>
-                    <button>Go to Login</button>
-                </Link>
-                <div>
-                    <FileUpload />
-                    <Query />
+            <div className="ChangePasswordForm">
+                <form>
+                    <input 
+                        id='oldPassword'
+                        type='password'
+                        placeholder='Password' 
+                        value={this.state.oldPassword}
+                        onChange={e => this.updateChangePasswordPage(e)}
+                    />
+                    <br />
+                    <input 
+                        id='newPassword'
+                        type='password'
+                        placeholder='New Password' 
+                        value={this.state.newPassword}
+                        onChange={e => this.updateChangePasswordPage(e)}
+                    />
+                    <br />
+                    <br />
+                    <input 
+                        id='retypeNewPassword'
+                        type='password'
+                        placeholder='Retype Password' 
+                        value={this.state.retypeNewPassword}
+                        onChange={e => this.updateChangePasswordPage(e)}
+                    />
+                    <br />
+                    <button onClick={e => this.onSubmit(e)}>Submit</button>
+                </form>
+                <div className={'nav-items'}>
+                    <Link to={place}>
+                        <button>Confirm</button>
+                    </Link>
                 </div>
             </div>
         )
-    }
-
+    }   
 }
