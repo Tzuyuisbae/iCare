4121b9de730c95e0bc88f8ae0af27d2269444a0e
saioduri <odurisai@gmail.com>
2018-11-21 15:14:09 -0500

    Completed Upload Page Style

--- icare/src/pages/UploadPage.js
+++ icare/src/pages/UploadPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/main.css';
+import '../css/login.css';
 import FileUpload from '../components/fileUpload.js';
 import Query from '../components/query.js';
 import Page from '../Page.js';
@@ -10,18 +11,26 @@ export default class UploadPage extends React.Component {
 
     render() {
         return (
+            <div>
+            <div className="topleft">
+                <p className="iCareWhite">iCare</p>
+            </div>
             <Page id="upload" className="fullblue">
-                <div className="upload">
-                    <h1>upload page is here haha yeet</h1>
-                    <Link to={'/'}>
-                        <button>Go to Login</button>
-                    </Link>
-                    <div>
-                        <FileUpload />
-                        <Query />
+                <div className="pos">
+                    <div className="form">
+                        <div className="col2">
+                                <h1 className="signupText">Fill Out The Form</h1>
+                                <Link to={'/'}>
+                                    <button>Go to Login</button>
+                                </Link>
+                                <FileUpload />
+                                <Query />
+                                <button className="button">Submit</button>
+                        </div>
                     </div>
                 </div>
             </Page>
+            </div>
         )
     }
 
